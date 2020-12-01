c0682b346cb869c404d2ab2bc1bcf8def69d9787
Scouser008 <alexguldbrand@gmail.com>
2020-12-01 20:50:33 +0100

    Adding sampling option

--- R/functions.R
+++ R/functions.R
@@ -9,19 +9,22 @@
 #' @param df data.frame
 #' @param x `str` name of features in `df`
 #' @param y `str` target feature
+#' @param sample_size `int` random sample taken from `df` to speed
+#' up calculations. If *NULL* all samples are used.
 #' @param cv_folds number of cross validations folds
 #' @param repeated_cv `int` number of repeated cross validations
 #'
 #' @return A list with the predictive power score for each feature in `x`
 #'
-pp_score <- function(df, x, y, cv_folds = 5, repeated_cv = 1) {
+pp_score <- function(df, x, y, sample_size = NULL,
+                     cv_folds = 5, repeated_cv = 1) {
 
   stopifnot(is.data.frame(df), c(x, y) %in% names(df))
 
   results <- list()
 
   for (i in x) {
-    results[i] <- score(df, i, y, cv_folds, repeated_cv)
+    results[i] <- score(df, i, y, sample_size, cv_folds, repeated_cv)
   }
 
   return(results)
@@ -40,34 +43,41 @@ pp_score <- function(df, x, y, cv_folds = 5, repeated_cv = 1) {
 #' @param df `data.frame` input data which contains `x` and `y`
 #' @param x `str` name of feature
 #' @param y `str` name of target
+#' @param sample_size `int` random sample taken from `df` to speed
+#' up calculations. If *NULL* all samples are used.
 #' @param cv_folds `int` number of cross validations folds
 #' @param repeated_cv `int` number of repeated cross validations
 #'
 #' @importFrom rpart rpart
 #' @importFrom MLmetrics AUC
 #'
-#' @return The predictive power score.
+#' @return The predictive power score for `x`.
 #'
-score <- function(df, x, y, cv_folds = 5L, repeated_cv = 1L) {
+score <- function(df, x, y, sample_size = NULL, cv_folds = 5L,
+                  repeated_cv = 1L) {
 
   stopifnot(is.numeric(df[[x]]), length(unique(df[[y]])) == 2)
 
   # removing NA
   df <- na.omit(df[c(x, y)])
+  if (nrow(df) == 0) stop("Zero rows in data after removing NA's!")
 
   # looping over repeated cv and cv folds
   results <- list()
 
   for(j in 1:repeated_cv) {
 
+    # Sampling data
+    df_sampled <- sample_data(df, sample_size)
+
     # Identifying cross validations folds
-    cv <- sample(1:cv_folds, size = nrow(df), replace = TRUE)
+    cv <- sample(1:cv_folds, size = nrow(df_sampled), replace = TRUE)
 
     for (i in 1:cv_folds) {
 
       # Splitting data
-      train <- data[cv != i, c(x, y)]
-      test <- data[cv == i, c(x, y)]
+      train <- df_sampled[cv != i, c(x, y)]
+      test <- df_sampled[cv == i, c(x, y)]
 
       # Model
       fit <- rpart(paste(y, "~", x), data = train, method = "class")
@@ -87,3 +97,22 @@ score <- function(df, x, y, cv_folds = 5L, repeated_cv = 1L) {
 
 }
 
+# sample_data -----------------------------------------------------------------
+#' @title sample_data
+#'
+#' @description Takes a random sample from `df` of size `sample_size`.
+#'
+#' @param df `data.frame` input data to sample from
+#' @param sample_size `int` or `NULL` number of samples to take from `df`.
+#'
+#' @return `data.frame` with samples from `df` or `df` if `sample_size`
+#' is *NULL*.
+#'
+sample_data <- function(df, sample_size) {
+
+  if (!is.null(sample_size))  {
+    df <- df[sample(df, sample_size), ]
+  }
+
+  return(df)
+}
