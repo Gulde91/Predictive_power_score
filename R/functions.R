a8c41478b53ffd8f914ea94992a8a13db2ea5048
Scouser008 <alexguldbrand@gmail.com>
2020-12-01 18:22:45 +0100

    Adding pp score functions across multiple features

--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,38 @@
 
+# pp_score --------------------------------------------------------------------
+#' @title pp_score
+#'
+#' @description Calculates predictive power score for `x` predicts `y`. Where
+#' `x` can be multiple features in `df`. This is a light (wip) implementation
+#' which for now only supports binary classification and x as numeric features.
+#'
+#' @param df data.frame
+#' @param x `str` name of features in `df`
+#' @param y `str` target feature
+#' @param cv_folds number of cross validations folds
+#'
+#' @return A list with the predictive power score for each feature in `x`
+#'
+pp_score <- function(df, x, y, cv_folds) {
+
+  stopifnot(is.data.frame(df), c(x, y) %in% names(df))
+
+  results <- list()
+
+  for (i in x) {
+    results[i] <- score(df, i, y, cv_folds)
+  }
+
+  return(results)
+}
+
 
 # score -----------------------------------------------------------------------
 #' @title Calculates predictive power score
 #'
 #' @description Calculates predictive power score for `x` predicts `y`. This is
-#' a light wip implementation.
+#' a light (wip) implementation which for now only supports binary
+#' classification and x as numeric a feature.
 #'
 #' @param df `data.frame` input data which contains `x` and `y`
 #' @param x `str` name of feature
@@ -18,7 +46,7 @@
 #'
 score <- function(df, x, y, cv_folds = 5L) {
 
-  stopifnot(is.data.frame(df), c(x, y) %in% names(df))
+  stopifnot(is.numeric(df[[x]]), length(unique(df[[y]])) == 2)
 
   # removing NA
   df <- na.omit(df[c(x, y)])
