9d05ce4521d2af5e7cd676f5df544ae19335e2f9
Scouser008 <alexguldbrand@gmail.com>
2020-12-01 19:16:49 +0100

    Adding repeated cv

--- R/functions.R
+++ R/functions.R
@@ -10,17 +10,18 @@
 #' @param x `str` name of features in `df`
 #' @param y `str` target feature
 #' @param cv_folds number of cross validations folds
+#' @param repeated_cv `int` number of repeated cross validations
 #'
 #' @return A list with the predictive power score for each feature in `x`
 #'
-pp_score <- function(df, x, y, cv_folds) {
+pp_score <- function(df, x, y, cv_folds = 5, repeated_cv = 1) {
 
   stopifnot(is.data.frame(df), c(x, y) %in% names(df))
 
   results <- list()
 
   for (i in x) {
-    results[i] <- score(df, i, y, cv_folds)
+    results[i] <- score(df, i, y, cv_folds, repeated_cv)
   }
 
   return(results)
@@ -34,47 +35,52 @@ pp_score <- function(df, x, y, cv_folds) {
 #' a light (wip) implementation which for now only supports binary
 #' classification and x as numeric a feature.
 #'
+#' @details for mere information: https://github.com/8080labs/ppscore
+#'
 #' @param df `data.frame` input data which contains `x` and `y`
 #' @param x `str` name of feature
 #' @param y `str` name of target
 #' @param cv_folds `int` number of cross validations folds
+#' @param repeated_cv `int` number of repeated cross validations
 #'
 #' @importFrom rpart rpart
 #' @importFrom MLmetrics AUC
 #'
 #' @return The predictive power score.
 #'
-score <- function(df, x, y, cv_folds = 5L) {
+score <- function(df, x, y, cv_folds = 5L, repeated_cv = 1L) {
 
   stopifnot(is.numeric(df[[x]]), length(unique(df[[y]])) == 2)
 
   # removing NA
   df <- na.omit(df[c(x, y)])
 
-  # Identifying cross validations folds
-  cv <- sample(1:cv_folds, size = nrow(df), replace = TRUE)
-
+  # looping over repeated cv and cv folds
   results <- list()
 
-  for (i in 1:cv_folds) {
+  for(j in 1:repeated_cv) {
 
-    # Splitting data
-    train <- data[cv != i, c(x, y)]
-    test <- data[cv == i, c(x, y)]
+    # Identifying cross validations folds
+    cv <- sample(1:cv_folds, size = nrow(df), replace = TRUE)
 
-    # Model
-    fit <- rpart(paste(y, "~", x), data = train, method = "class")
+    for (i in 1:cv_folds) {
 
-    # Predictions
-    pred <- predict(fit, test)[, 2]
+      # Splitting data
+      train <- data[cv != i, c(x, y)]
+      test <- data[cv == i, c(x, y)]
 
-    # Calculation roc auc
-    roc_auc <- AUC(pred, test[[y]])
+      # Model
+      fit <- rpart(paste(y, "~", x), data = train, method = "class")
 
-    results[i] <- roc_auc
+      # Predictions
+      pred <- predict(fit, test)[, 2]
 
-    #f1 <- F1_Score(test[[y]], pred)
-    # ConfusionDF(pred, test[[y]])
+      # Calculation roc auc
+      results[[as.character(j)]][i] <- AUC(pred, test[[y]])
+
+      #f1 <- F1_Score(test[[y]], pred)
+      # ConfusionDF(pred, test[[y]])
+    }
   }
 
   mean(unlist(results))
